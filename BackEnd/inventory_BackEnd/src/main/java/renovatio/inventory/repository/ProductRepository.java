0741f1d082ec9d5ae9d01c8d23752d364e9bdc84
Felipe Cano <78998642+Renovatio-V@users.noreply.github.com>
2024-07-19 14:41:47 -0500

    Delete BackEnd directory

--- BackEnd/inventory_BackEnd/src/main/java/renovatio/inventory/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package renovatio.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import renovatio.inventory.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
